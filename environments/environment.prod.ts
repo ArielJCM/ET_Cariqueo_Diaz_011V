c82122dc0a096bfe3076b900efdae41f39ec32d8
ArielJCM <arielcariqueo@gmail.com>
2024-12-18 02:47:24 -0300

    commit app_profe3

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'http://localhost:3000'
+  apiUrl:'https://et-cariqueo-diaz-011v.onrender.com/'
 };
--- environments/environment.ts
+++ environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://localhost:3000'
+  apiUrl:'https://et-cariqueo-diaz-011v.onrender.com/'
 };
 
 /*
